13ec6c28915710c456a916afdb48bff8138944a9
wagnert <wagner_tim78@hotmail.com>
2017-02-07 06:18:58 +0100

    Separating artefact export functionality into an interface/trait

---
+++ src/Subjects/ExportableSubjectInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * TechDivision\Import\Subjects\ExportableSubjectInterface
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import
+ * @link      http://www.techdivision.com
+ */
+
+namespace TechDivision\Import\Subjects;
+
+/**
+ * The interface for all subject implementations that supports an artefact export.
+ *
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import
+ * @link      http://www.techdivision.com
+ */
+interface ExportableSubjectInterface
+{
+
+    /**
+     * Export's the artefacts to CSV files.
+     *
+     * @param integer $timestamp The timestamp part of the original import file
+     * @param string  $counter   The counter part of the origin import file
+     *
+     * @return void
+     */
+    public function export($timestamp, $counter);
+}
