657d4cc553da053248c3b6b9ea7e6fdfffe4da9f
wagnert <wagner_tim78@hotmail.com>
2017-04-13 16:26:24 +0200

    Add symfony DI configuration

---
+++ src/Utils/EntityTypeCodes.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * TechDivision\Import\Utils\EntityTypeCodes
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import
+ * @link      http://www.techdivision.com
+ */
+
+namespace TechDivision\Import\Utils;
+
+/**
+ * Utility class containing the available page layout keys.
+ *
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import
+ * @link      http://www.techdivision.com
+ */
+class EntityTypeCodes
+{
+
+    /**
+     * This is a utility class, so protect it against direct
+     * instantiation.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * This is a utility class, so protect it against cloning.
+     *
+     * @return void
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * Key for the product entity 'catalog_product'.
+     *
+     * @var integer
+     */
+    const CATALOG_PRODUCT = 'catalog_product';
+
+    /**
+     * Key for the category entity 'catalog_category'.
+     *
+     * @var integer
+     */
+    const CATALOG_CATEGORY = 'catalog_category';
+}
