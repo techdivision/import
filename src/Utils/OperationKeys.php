fbf911d025d04fae8b8e94f5f6be3c0e2c3e7dc1
wagnert <wagner_tim78@hotmail.com>
2017-04-13 23:17:56 +0200

    Add symfony DI configuration

---
+++ src/Utils/OperationKeys.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * TechDivision\Import\Utils\OperationKeys
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import
+ * @link      http://www.techdivision.com
+ */
+
+namespace TechDivision\Import\Utils;
+
+/**
+ * A utility class for the available operations.
+ *
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import
+ * @link      http://www.techdivision.com
+ */
+class OperationKeys
+{
+
+    /**
+     * This is a utility class, so protect it against direct
+     * instantiation.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * This is a utility class, so protect it against cloning.
+     *
+     * @return void
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * The key for 'add-update' operation.
+     *
+     * @var string
+     */
+    const ADD_UPDATE = 'add-update';
+
+    /**
+     * The key for 'replace' operation.
+     *
+     * @var string
+     */
+    const REPLACE = 'replace';
+
+    /**
+     * The key for 'delete' operation.
+     *
+     * @var string
+     */
+    const DELETE = 'delete';
+}
