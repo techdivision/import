168834517f5f4c159c6c253e384097966c60c2db
Martin Eisenführer <m.eisenfuehrer@techdivision.com>
2020-10-15 18:42:16 +0200

    Change doc block second try

--- src/Loggers/GenericLogHandlerFactory.php
+++ src/Loggers/GenericLogHandlerFactory.php
@@ -11,8 +11,8 @@
  *
  * PHP version 5
  *
- * @author    Tim Wagner <t.wagner@techdivision.com>
- * @copyright 2019 TechDivision GmbH <info@techdivision.com>
+ * @author    Martin Eisenführer <m.eisenfuehrer@techdivision.com>
+ * @copyright 2020 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      https://github.com/techdivision/import
  * @link      http://www.techdivision.com
