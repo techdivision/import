6e590870ba1e8315ee1a6c9b658974dd9e4b56be
Martin Eisenführer <m.eisenfuehrer@techdivision.com>
2021-01-29 16:39:07 +0100

    Optimmize date convert with standard DateTime format

--- src/Subjects/I18n/SimpleDateConverter.php
+++ src/Subjects/I18n/SimpleDateConverter.php
@@ -83,15 +83,16 @@ class SimpleDateConverter implements DateConverterInterface
      */
     public function convert($date, $format = 'Y-m-d H:i:s')
     {
-
         // create a DateTime instance from the passed value
-        if ($dateTime = new \DateTime($date)) {
+        if ($dateTime = \DateTime::createFromFormat($this->getDateConverterConfiguration()->getSourceDateFormat(), $date)) {
             return $dateTime->format($format);
         }
 
-        // no default Format? Try format from converter config
-        if ($dateTime = \DateTime::createFromFormat($this->getDateConverterConfiguration()->getSourceDateFormat(), $date)) {
-            return $dateTime->format($format);
+        // Date is not in configured format? Try if is a default format
+        try {
+            return (new \DateTime($date))->format($format);
+        } catch( \Exception $e) {
+            // Catch if $date is no default date format
         }
 
         // return NULL, if the passed value is NOT a valid date
