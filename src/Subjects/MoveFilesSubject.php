5efa9d13d6e215a447f087376101238731900a32
Tim Wagner <t.wagner@techdivision.com>
2021-10-18 12:31:07 +0200

    Extend configuration for renaming files functionality

--- src/Subjects/MoveFilesSubject.php
+++ src/Subjects/MoveFilesSubject.php
@@ -14,7 +14,12 @@
 
 namespace TechDivision\Import\Subjects;
 
+use League\Event\EmitterInterface;
+use Doctrine\Common\Collections\Collection;
 use TechDivision\Import\Utils\RegistryKeys;
+use TechDivision\Import\Loaders\LoaderInterface;
+use TechDivision\Import\Services\RegistryProcessorInterface;
+use TechDivision\Import\Utils\Generators\GeneratorInterface;
 
 /**
  * The subject implementation to move the files to their target directory.
@@ -27,6 +32,37 @@ use TechDivision\Import\Utils\RegistryKeys;
  */
 class MoveFilesSubject extends AbstractSubject
 {
+
+    /**
+     *The loader for the proposed filename.
+     *
+     * @var \TechDivision\Import\Loaders\LoaderInterface
+     */
+    protected $proposedFilenameLoader;
+
+    /**
+     * Initialize the subject instance.
+     *
+     * @param \TechDivision\Import\Services\RegistryProcessorInterface $registryProcessor          The registry processor instance
+     * @param \TechDivision\Import\Utils\Generators\GeneratorInterface $coreConfigDataUidGenerator The UID generator for the core config data
+     * @param \Doctrine\Common\Collections\Collection                  $systemLoggers              The array with the system loggers instances
+     * @param \League\Event\EmitterInterface                           $emitter                    The event emitter instance
+     * @param \TechDivision\Import\Loaders\LoaderInterface             $proposedFilenameLoader     The loader for the proposed filename
+     */
+    public function __construct(
+        RegistryProcessorInterface $registryProcessor,
+        GeneratorInterface $coreConfigDataUidGenerator,
+        Collection $systemLoggers,
+        EmitterInterface $emitter,
+        LoaderInterface $proposedFilenameLoader
+    ) {
+
+        // pass the instances to the parent constructor
+        parent::__construct($registryProcessor, $coreConfigDataUidGenerator, $systemLoggers, $emitter);
+
+        // initialize the proposed filename loader
+        $this->proposedFilenameLoader = $proposedFilenameLoader;
+    }
 
     /**
      * Clean up the global data after importing the variants.
@@ -80,19 +116,6 @@ class MoveFilesSubject extends AbstractSubject
     }
 
     /**
-     * Return's the next source directory, which will be the target directory
-     * of this subject, in most cases.
-     *
-     * @param string $serial The serial of the actual import
-     *
-     * @return string The new source directory
-     */
-    public function getNewSourceDir($serial)
-    {
-        return sprintf('%s/%s', $this->getConfiguration()->getSourceDir(), $serial);
-    }
-
-    /**
      * Imports the content of the file with the passed filename.
      *
      * @param string $serial   The serial of the actual import
@@ -108,13 +131,19 @@ class MoveFilesSubject extends AbstractSubject
         $this->setSerial($serial);
         $this->setFilename($filename);
 
-        // query whether the new source directory has to be created or not
-        if (!$this->isDir($newSourceDir = $this->getNewSourceDir($serial))) {
-            $this->mkdir($newSourceDir);
-        }
+        // prepare the new filename, e. g. in case a specific filename has been
+        // passed on the command line as second argument
+        $newFilename = $this->getNewFilename($filename);
+
+        // prepare the new source directory
+        $newSourceDir = $this->prepareNewSourceDir($serial);
 
         // move the file to the new source directory
-        $this->rename($filename, sprintf('%s/%s', $newSourceDir, sprintf('product-import_%s_01.csv', date('Ymd-His')) /* basename($filename) */));
+        $this->rename($filename, $target = sprintf('%s/%s', $newSourceDir, $newFilename));
+
+        // log a message that the original file has been moved
+        // to the target directory, and probably renamed
+        $this->getSystemLogger()->notice(sprintf('Moved file %s to %s', $filename, $target));
 
         // update the status
         $this->mergeStatus(
@@ -132,5 +161,84 @@ class MoveFilesSubject extends AbstractSubject
                 )
             )
         );
+    }
+
+    /**
+     * Return's the next source directory, which will be the target directory
+     * of this subject, in most cases.
+     *
+     * @param string $serial The serial of the actual import
+     *
+     * @return string The new source directory
+     */
+    protected function getNewSourceDir(string $serial) : string
+    {
+        return sprintf('%s/%s', $this->getConfiguration()->getTargetDir(), $serial);
+    }
+
+    /**
+     * Prepares the new source directory based on the passed serial.
+     *
+     * @param string $serial The serial to prepare the new source directory with
+     *
+     * @return string The new source directory
+     */
+    protected function prepareNewSourceDir(string $serial) : string
+    {
+
+        // initialize the path for the new source directory
+        $newSourceDir = $this->getNewSourceDir($serial);
+
+        // query whether the new source directory has to be created or not
+        if ($this->isDir($newSourceDir) === false) {
+            $this->mkdir($newSourceDir);
+        }
+
+        // return the new source directory
+        return $newSourceDir;
+    }
+
+    /**
+     * Return's the loader for the proposed filename.
+     *
+     * @return \TechDivision\Import\Loaders\LoaderInterface The proposed filename loader
+     */
+    protected function getProposedFilenameLoader() : LoaderInterface
+    {
+        return $this->proposedFilenameLoader;
+    }
+
+    /**
+     * Prepares and returns the new filename in case a file has been specified
+     * as second commanline line argument.
+     *
+     * @param string $filename
+     *
+     * @return string
+     */
+    protected function getNewFilename(string $filename) : string
+    {
+
+        // extract the new filename from the path
+        $newFilename = basename($filename);
+
+        // load the subject and the global configuration
+        $subjectConfiguration = $this->getConfiguration();
+        $configuration = $subjectConfiguration->getConfiguration();
+
+        // query whether or not specifc filename has been passed as command
+        // line argument, if yes make sure it is the passed one
+        if ($configuration->getFilename() === realpath($filename)) {
+            // load the first prefixed subject, because we've to prepare
+            // a filename that matches this fileresolver configuration
+            /** @var \TechDivision\Import\Configuration\SubjectConfigurationInterface $firstPrefixedSubject */
+            $firstPrefixedSubject = $configuration->getFirstPrefixedSubject();
+            // prepare the proposed filename based on the filresolver configuration
+            $this->getProposedFilenameLoader()->setFileResolverConfiguration($firstPrefixedSubject->getFileResolver());
+            $newFilename = $this->getProposedFilenameLoader()->load($newFilename);
+        }
+
+        // return the original filename
+        return $newFilename;
     }
 }
