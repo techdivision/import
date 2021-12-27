33a612cbbb04c33b283b516543f26311965c99c2
Kenza Yamlahi <k.yamlahi@techdivision.com>
2021-12-27 16:20:19 +0100

    extend callback for associated skus for no strict mode

--- src/Callbacks/CommaDelimiterSkuRelationsValidatorCallback.php
+++ src/Callbacks/CommaDelimiterSkuRelationsValidatorCallback.php
@@ -15,6 +15,7 @@
 namespace TechDivision\Import\Callbacks;
 
 use TechDivision\Import\Utils\ColumnKeys;
+use TechDivision\Import\Utils\RegistryKeys;
 
 /**
  * A callback implementation that validates the a list of values.
@@ -63,6 +64,25 @@ class CommaDelimiterSkuRelationsValidatorCallback extends ArrayValidatorCallback
             array_push($skuErrors, $value);
         }
         if (count($skuErrors) > 0) {
+            if (!$this->getSubject()->isStrictMode()) {
+                $this->getSubject()->mergeStatus(
+                    array(
+                        RegistryKeys::NO_STRICT_VALIDATIONS => array(
+                            basename($this->getSubject()->getFilename()) => array(
+                                $this->getSubject()->getLineNumber() => array(
+                                    $attributeCode  =>  sprintf(
+                                        'Found invalid SKUs "%s" to be related to %s product with SKU "%s"',
+                                        implode(',', $skuErrors),
+                                        $rowProductType,
+                                        $rowSku
+                                    )
+                                )
+                            )
+                        )
+                    )
+                );
+                return;
+            }
             // throw an exception if the value is NOT in the array
             throw new \InvalidArgumentException(
                 sprintf(
