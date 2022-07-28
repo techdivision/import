e538bdc820d6336bb014ba29f7a9c89e8106d013
Kenza Yamlahi <k.yamlahi@techdivision.com>
2022-07-28 11:58:02 +0200

    PAC-353: integrate new config to read data from api

--- src/Repositories/CoreConfigDataRepository.php
+++ src/Repositories/CoreConfigDataRepository.php
@@ -14,11 +14,12 @@
 
 namespace TechDivision\Import\Repositories;
 
+use TechDivision\Import\Dbal\Collection\Repositories\AbstractFinderRepository;
+use TechDivision\Import\Dbal\Repositories\Finders\FinderFactoryInterface;
 use TechDivision\Import\Utils\SqlStatementKeys;
 use TechDivision\Import\Utils\Generators\GeneratorInterface;
 use TechDivision\Import\Dbal\Connection\ConnectionInterface;
 use TechDivision\Import\Dbal\Repositories\SqlStatementRepositoryInterface;
-use TechDivision\Import\Dbal\Collection\Repositories\AbstractRepository;
 
 /**
  * Repository implementation to load the Magento 2 configuration data.
@@ -29,7 +30,7 @@ use TechDivision\Import\Dbal\Collection\Repositories\AbstractRepository;
  * @link      https://github.com/techdivision/import
  * @link      http://www.techdivision.com
  */
-class CoreConfigDataRepository extends AbstractRepository implements CoreConfigDataRepositoryInterface
+class CoreConfigDataRepository extends AbstractFinderRepository implements CoreConfigDataRepositoryInterface
 {
 
     /**
@@ -47,19 +48,23 @@ class CoreConfigDataRepository extends AbstractRepository implements CoreConfigD
     protected $coreConfigDataStmt;
 
     /**
-     * Initialize the repository with the passed connection and utility class name.
-     * .
-     *
-     * @param \TechDivision\Import\Utils\Generators\GeneratorInterface               $coreConfigDataUidGenerator The UID generator for the core config data
-     * @param \TechDivision\Import\Dbal\Connection\ConnectionInterface               $connection                 The connection instance
-     * @param \TechDivision\Import\Dbal\Repositories\SqlStatementRepositoryInterface $sqlStatementRepository     The SQL repository instance
+     * @var array
+     */
+    protected $coreConfigData = array();
+
+    /**
+     * @param GeneratorInterface              $coreConfigDataUidGenerator The coreConfigDataUidGenerator factory instance
+     * @param ConnectionInterface             $connection                 The connection factory instance
+     * @param SqlStatementRepositoryInterface $sqlStatementRepository     The SQL repository instance
+     * @param FinderFactoryInterface          $finderFactory              The finder factory instance
      */
     public function __construct(
         GeneratorInterface $coreConfigDataUidGenerator,
         ConnectionInterface $connection,
-        SqlStatementRepositoryInterface $sqlStatementRepository
+        SqlStatementRepositoryInterface $sqlStatementRepository,
+        FinderFactoryInterface $finderFactory
     ) {
-        parent::__construct($connection, $sqlStatementRepository);
+        parent::__construct($connection, $sqlStatementRepository, $finderFactory);
         $this->coreConfigDataUidGenerator = $coreConfigDataUidGenerator;
     }
 
@@ -70,10 +75,10 @@ class CoreConfigDataRepository extends AbstractRepository implements CoreConfigD
      */
     public function init()
     {
-
         // initialize the prepared statements
         $this->coreConfigDataStmt =
             $this->getConnection()->prepare($this->loadStatement(SqlStatementKeys::CORE_CONFIG_DATA));
+        $this->addFinder($this->finderFactory->createFinder($this, SqlStatementKeys::CORE_CONFIG_DATA));
     }
 
     /**
@@ -83,25 +88,54 @@ class CoreConfigDataRepository extends AbstractRepository implements CoreConfigD
      */
     public function findAll()
     {
-
-        // prepare the core configuration data
-        $coreConfigDatas = array();
-
         // execute the prepared statement
         $this->coreConfigDataStmt->execute();
 
-        // load the available core config data
-        $availableCoreConfigDatas = $this->coreConfigDataStmt->fetchAll(\PDO::FETCH_ASSOC);
+        // load the available core config data from database
+        $availableCoreConfigData = $this->coreConfigDataStmt->fetchAll(\PDO::FETCH_ASSOC);
 
-        // create the array with the resolved category path as keys
-        foreach ($availableCoreConfigDatas as $coreConfigData) {
+        // load the available core config data from magento instance
+        $availableCoreConfigApiData = $this->getFinder(SqlStatementKeys::CORE_CONFIG_DATA)->find();
+
+        $availableCoreConfigData = array_merge($availableCoreConfigData, $availableCoreConfigApiData);
+
+        // Set core config data from api and db
+        $this->setCoreConfigData($availableCoreConfigData);
+
+        // return array with the configuration data
+        return $this->coreConfigData;
+    }
+
+    /**
+     * create the array with the resolved category path as keys
+     *
+     * @param array $availableCoreConfigData the available Core ConfigData
+     *
+     * @return void
+     */
+    protected function setCoreConfigData($availableCoreConfigData)
+    {
+        foreach ($availableCoreConfigData as $coreConfigData) {
             // prepare the unique identifier
             $uniqueIdentifier = $this->coreConfigDataUidGenerator->generate($coreConfigData);
             // append the config data value with the unique identifier
-            $coreConfigDatas[$uniqueIdentifier] = $coreConfigData;
+            $this->coreConfigData[$uniqueIdentifier] = $coreConfigData;
         }
+    }
 
-        // return array with the configuration data
-        return $coreConfigDatas;
+    /**
+     * @return string
+     */
+    public function getPrimaryKeyName()
+    {
+        return \TechDivision\Import\Utils\MemberNames::PATH;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEntityName()
+    {
+        return '';
     }
 }
