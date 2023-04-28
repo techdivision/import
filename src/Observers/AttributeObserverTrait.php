f7cef4092e0425421662072a3db5f48390628471
Lukas Kiederle <l.kiederle@techdivision.com>
2023-04-28 13:28:52 +0200

    fixed Issue where not required attributes would not be skipped with php74 because of type missmatch with same value

--- src/Observers/AttributeObserverTrait.php
+++ src/Observers/AttributeObserverTrait.php
@@ -434,7 +434,7 @@ trait AttributeObserverTrait
         // query whether or not the colunm IS empty and it is NOT in the
         // array with the default column values, because in that case we
         // want to skip processing the attribute
-        if (array_key_exists($this->attributeCode, $this->getDefaultColumnValues()) === false && ($this->attributeValue === '' || $this->attributeValue == null) && $this->attribute['is_required'] === 0) {
+        if (array_key_exists($this->attributeCode, $this->getDefaultColumnValues()) === false && ($this->attributeValue === '' || $this->attributeValue == null) && !$this->attribute['is_required']) {
             $this->operation = OperationNames::SKIP;
         }
 
